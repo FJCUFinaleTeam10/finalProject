71d97db3a487eaf8c876f7cfaefc1ac2367507c9
DanielDu <danghoangnhan.1@gmail.com>
2021-06-04 20:38:15 +0800

    fix the reference problem in findVehicle module

--- findVehicle.py
+++ findVehicle.py
@@ -1,3 +1,5 @@
+import copy
+
 from comtypes.automation import _
 
 from Math.distance import distance
@@ -6,16 +8,20 @@ from model.order import Ds
 from model.restaurant import restaurant
 
 
-def tripTime(driver: driver, res: restaurant, order: Ds):
-    return distance(driver.x, driver.y, res.xPosition, res.yPosition) + \
-           distance(res.xPosition, res.yPosition, order.x, order.y)
+def tripTime(driv: driver, res: restaurant, order: Ds):
+    try:
+        return distance(driv.x, driv.y, res.xPosition, res.yPosition) + \
+               distance(res.xPosition, res.yPosition, order.x, order.y)
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 
 
-def FindVehicle(Theta_hat, Order, buffer, driverList,restaurantList):
+
+def FindVehicle(Theta_hat, Order, buffer, driverList, restaurantList):
     OrderRestaurant = restaurantList[Order.R + 1]
     minTimeDriver = driverList[0]
 
     for currentDriver in driverList:
         if tripTime(currentDriver, OrderRestaurant, Order) < tripTime(minTimeDriver, OrderRestaurant, Order):
-            minTimeDriver = driver
+            minTimeDriver = copy.copy(currentDriver)
     return minTimeDriver
